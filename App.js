378bd8606fdff5d9615ffb35055c6e6c666f9943
Thaís Falcão <101199332+thaisfalcao@users.noreply.github.com>
2023-01-17 14:15:19 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import IssLocationScreen from "./screens/IssLocation";
+import MeteorScreen from "./screens/Meteors";
+import UpdateScreen from "./screens/Updates";
+
+
+
+export default function App() {
+  return (
+
+  );
+}
